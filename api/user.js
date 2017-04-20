0d831d789cb624c320ab7290ad337a92458e95f3
shilong.sang <i5ting@126.com>
2017-04-20 20:29:36 +0800

    lazycommmit, please ignore

---
+++ api/user.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "path": "/home1",
+    "body": {
+        status: {
+            code: 0,
+            msg: "sucess"
+        },
+        data: {
+            key: "Hello world!"
+        }
+    },
+}
---
+++ app.js
@@ -0,0 +1,9 @@
+var apie = require('./apie').app;
+
+// return an express app
+var app = apie('.');
+
+app.set('view engine', 'pug');
+
+// start server
+app.listen(3000)
